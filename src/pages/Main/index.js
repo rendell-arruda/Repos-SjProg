9895b3562e1a87452df191f26b3ad99089eb2f17
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-09 08:46:20 -0300

    add function delete

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
-import { Container, Form, SubmitButton } from './styles';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import { FaBars, FaGithub, FaPlus, FaSpinner, FaTrash } from 'react-icons/fa';
 import api from '../../services/api';
 
 export default function Main() {
@@ -40,6 +40,15 @@ export default function Main() {
     setNewRepo(e.target.value);
   }
 
+  const handleDelete = useCallback(
+    repo => {
+      //
+      const find = repositorios.filter(r => r.name !== repo);
+      setRepositorios(find);
+    },
+    [repositorios]
+  );
+
   return (
     <Container>
       <h1>
@@ -62,6 +71,23 @@ export default function Main() {
           )}
         </SubmitButton>
       </Form>
+
+      <List>
+        {repositorios.map(repo => (
+          // O key é para o react identificar qual item é qual
+          <li key={repo.name}>
+            <span>
+              <DeleteButton onClick={() => handleDelete(repo.name)}>
+                <FaTrash size={14} />
+              </DeleteButton>
+              {repo.name}
+            </span>
+            <a href="">
+              <FaBars size={20} />
+            </a>
+          </li>
+        ))}
+      </List>
     </Container>
   );
 }
