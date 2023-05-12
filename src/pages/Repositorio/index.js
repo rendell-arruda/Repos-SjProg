2083676861a7e3d74ea7635df22442946bd67977
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-12 09:41:17 -0300

    puxa as isses

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Owner, Loading, BackButton } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
 
 import api from '../../services/api';
@@ -33,6 +33,7 @@ export default function Repositorio({ match }) {
       //atualizar os estados
       setStateRepositorio(repositorioData.data);
       setIssues(issuesData.data);
+      console.log(issuesData.data);
       setLoading(false);
     }
     load();
@@ -54,6 +55,23 @@ export default function Repositorio({ match }) {
         <h1>{stateRepositorio.name}</h1>
         <p>{stateRepositorio.description}</p>
       </Owner>
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
